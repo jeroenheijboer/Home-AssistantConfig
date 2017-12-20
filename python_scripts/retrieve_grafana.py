941ce6270c2c83886d2b763f5b69cfbfd35db41c
Martinvdm <martin@vd-meer.com>
2017-12-20 23:08:45 +0100

    Changed Grafana-Camera images for solar panels (working)

--- python_scripts/retrieve_grafana.py
+++ python_scripts/retrieve_grafana.py
@@ -4,22 +4,44 @@ Download a rendered png from a panel in grafana into a subdirectory.
 
 subdir_name = 'grafana_graphs'
 
-# example parameters for downloader component
+# Wattage
 data = {
-    'url': 'http://grafanaviewer:GrafanaC00l@homeassistant.vd-meer.com:3000/render/dashboard-solo/db/zonnepanelen?orgId=1&panelId=2&theme=light&width=500&height=250',
+    'url': 'http://127.0.0.1:3000/render/dashboard-solo/db/zonnepanelen?orgId=1&panelId=1&theme=light&width=500&height=250',
     'subdir': subdir_name,
-    'filename': 'zonnepanelen_1_2',
+    'filename': 'zonnepanelen_1_1.png',
     'overwrite': True
 }
 
 # call  downloader component to save the file
 hass.services.call('downloader', 'download_file', data)
 
-# example parameters for downloader component
+# Opwekking Dag
 data = {
-    'url': 'http://grafanaviewer:GrafanaC00l@homeassistant.vd-meer.com:3000/render/dashboard-solo/db/zonnepanelen?orgId=1&panelId=1&theme=light&width=500&height=250',
+    'url': 'http://127.0.0.1:3000/render/dashboard-solo/db/opwekking-dag-kwh?orgId=1&panelId=1&theme=light&width=500&height=250',
     'subdir': subdir_name,
-    'filename': 'zonnepanelen_1_1',
+    'filename': 'zonnepanelen_1_2.png',
+    'overwrite': True
+}
+
+# call  downloader component to save the file
+hass.services.call('downloader', 'download_file', data)
+
+# Opwekking Week
+data = {
+    'url': 'http://127.0.0.1:3000/render/dashboard-solo/db/opwekking-week-kwh?orgId=1&panelId=1&theme=light&width=500&height=250',
+    'subdir': subdir_name,
+    'filename': 'zonnepanelen_1_3.png',
+    'overwrite': True
+}
+
+# call  downloader component to save the file
+hass.services.call('downloader', 'download_file', data)
+
+# Opwekking Maand
+data = {
+    'url': 'http://127.0.0.1:3000/render/dashboard-solo/db/opwekking-maand-kwh?orgId=1&panelId=1&theme=light&width=500&height=250',
+    'subdir': subdir_name,
+    'filename': 'zonnepanelen_1_4.png',
     'overwrite': True
 }
 
